6b531f831232b00e91ac44b3b7ab1e1ec31771ec
Joklr <gxy0082@gmail.com>
2021-03-12 09:07:29 +0800

    deploy.sh publish

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env sh
 
-set -e
+git add .
+git commit -m 'deploy.sh publish'
 
-yarn build
-
-cd docs/.vuepress/dist
-
-echo blog.joklr.org > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-git push -f --set-upstream git@github.com:OJoklrO/OJoklrO.github.io.git master
-
-cd -
-
-rm -rf docs/.vuepress/dist
+git pull origin master
+git push origin master
